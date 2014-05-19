d502d07596d04d5faa7f9d49c54ce0bd7c3e8d10
kevinchen93 <meejathoughts@gmail.com>
2014-05-18 20:31:54 -0400

    wtf

--- data/regressions.R
+++ data/regressions.R
@@ -11,16 +11,15 @@ wb <- read.csv(paste0(URB_PATH, "new_urban.csv"), check.names=FALSE)
 #' extract("AFG", "Urban population", "1970")
 #' extract("AFG", "Urban population", 1970)
 #' extract("AFG", "Urban population")
-#' @param country.code A string. Use grep(country.code, gtd$world_bank_code)
-#' to see if the string appears in the levels. If Blank string "" is level 1.
+#' @param country.code An integer level in gtd$world_bank_code.
 #' @param indicator.name A string.
 #' @param year An integer or string.
 extract <- function(country.code, indicator.name, year=NA) { 
-  # No World Bank country for this GTD country
-  if (country.code == "") 
+  # level 1, empty string, No World Bank country for this GTD country
+  print(country.code)
+  if (country.code == 1) 
     return(NA)
-  # country.code is not found
-  #  else if (length(grep(country.code, levels(gtd$world_bank_code))) == 0)
+  country.code = as.character(country.code)
   df = subset(subset(wb, Country.Code == country.code), Indicator.Name == indicator.name)
   if (is.na(year)) return(df)
   if (is.numeric(year)) 
@@ -33,8 +32,29 @@ INDICATORS <- c(
   "Urban population (% of total)",
   "Urban population growth (annual %)")
 
-View(extract("AFG", INDICATORS[1]))
+if ("data.table" %in% row.names(installed.packages())  == FALSE) install.packages("data.table") 
+require("data.table")
+new.gtd <- data.table(gtd, keep.rownames = TRUE)
+
+new.gtd$urban_growth <- NA
+names(new.gtd)[135]
+for (row in 1:10) {
+  wb_code = new.gtd[row,135]
+  print(wb_code)
+  year = new.gtd[row,]$iyear
+  #new.gtd[row, urban_growth := extract(wb_code, INDICATORS[3], year)]
+}
+View(head(new.gtd)$urban_growth)
 
 
-gtd$urban_growth <- NA
 gtd['urban_growth'] <- extract(gtd$world_bank_code, INDICATORS[3], gtd$iyear)
+
+
+DT <- data.table(x = 1:5, y = 6:10, z = 11:15)
+DT[2, y := DT[5, y]]
+DT
+for (row in 1:nrow(DT)) {
+  for (col in 1:ncol(DT)) {
+    DT[row, y := DT[row, z]]
+  }
+}
